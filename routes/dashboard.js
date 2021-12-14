805ce9687c1bfffb9d3e1594e08a0fa267fe03c4
yihong <beliefloatsky@gmail.com>
2021-12-14 14:43:03 +0800

    只刪了幾行東西

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -53,31 +53,6 @@ module.exports = app => {
         }
     })
 
-    router.get('/restaurant/:restaurantId', async (req, res) => {
-        try {
-            // 透過 id 到資料庫尋找相對應的用戶
-            const restaurant = await Restaurant.findById(req.params.restaurantId)
-            // 若無該用戶，或用戶無大頭貼，則丟出錯誤
-            if (!restaurant || !restaurant.menu) {
-                throw new Error()
-            }
-            // 設定回傳 Header 的資料類型為 png 格式的圖片
-            //res.set('Content-Type', 'image/png')
-            // 回傳大頭貼
-            res.send({
-                restaurantName: restaurant.restaurantName,
-                restaurantPhone: restaurant.restaurantPhone,
-                restaurantLocation: restaurant.restaurantLocation,
-                serviceHour: restaurant.serviceHour,
-                type: restaurant.type,
-                dish: restaurant.dish,
-                menu: "dashboard/images/" + restaurant._id
-            })
-        } catch (error) {
-            res.status(404).send()
-        }
-    })
-
     router.get('/restaurant/images/:restaurantId', async (req, res) => {
         try {
             // 透過 id 到資料庫尋找相對應的用戶
